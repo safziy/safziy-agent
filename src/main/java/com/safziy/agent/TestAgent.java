28675e00ec37074a7e284397769c51c691629c46
safziy <787395455@qq.com>
2015-12-31 11:05:10 +0800

    Commit

--- src/main/java/com/safziy/agent/TestAgent.java
+++ src/main/java/com/safziy/agent/TestAgent.java
@@ -3,11 +3,7 @@ package com.safziy.agent;
 import java.lang.instrument.Instrumentation;
 
 public class TestAgent {
-	public static Instrumentation inst = null;
-	
 	public static void premain(String agentArgument, Instrumentation instrumentation) {
 		System.out.println("Test Java Agent");
-		
-		inst = instrumentation;
 	}
 }
